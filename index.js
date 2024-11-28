9a2db62dec059a8e7423de4764eb1e06052d86ed
duckien <nguyenduckien0162@gmail.com>
2024-11-28 22:57:32 +0700

    test

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const mainContentLetter = "Chúng mình biết và chơi với nhau cũng 6 năm
 
 // Gắn 1 đường link ảnh bất kì
 let imgStart = document.querySelector(".myAI"); //Hình ảnh xuất hiện trong lời mở đầu của bức thư
-imgStart.src = "./img/cute-young-boy-kid-wearing-vest-and-hat-free-png.png";
+imgStart.src = "./img/Remove-bg.ai_1732803614790.png";
+
 
 // Gắn 1 link ảnh bất kì
 let imgLetter = document.querySelector(".img");
-imgLetter.src = "./img/b4bbdb54b7152338d7143cb444a77f09.png"; //Hình ảnh xuất hiện trong nội dung của bức thư sau khi bức thư được viết ra hết
+imgLetter.src = "./img/Remove-bg.ai_1732803745759.png"; //Hình ảnh xuất hiện trong nội dung của bức thư sau khi bức thư được viết ra hết
 
 const splitContentLetterSrart_actived = contentLetterSrart_actived.split("");
 
